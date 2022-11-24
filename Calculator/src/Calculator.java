b8183a365a309db5dd1a4dc442349a66a6e4e624
Daniel Silva <105682016+zegutin@users.noreply.github.com>
2022-11-23 21:50:46 -0300

    Add files via upload

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,113 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener{
+
+	JFrame frame;
+	JTextField texfiel;
+	JButton[] numberButtons = new JButton[10];
+	JButton[] functionButtons = new JButton[8];
+	JButton addButton,subButton,mulButton,divButton;
+	JButton decButton, equButton, delButton, clrButton;
+	JPanel panel;
+	
+	Font myFont = new Font("Ink Free",Font.BOLD,30);
+	
+	double num1=0,num2=0,result=0;
+	char operator;
+	
+	Calculator(){
+		
+		frame = new JFrame("Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,550);
+		frame.setLayout(null);
+
+		JTextField textfield = new JTextField();
+		textfield.setBounds(50,25,300,50);
+		textfield.setFont(myFont);
+		textfield.setEditable(false);;
+		
+		addButton = new JButton ("+");
+		subButton = new JButton ("-");
+		mulButton = new JButton ("x");
+		divButton = new JButton ("÷");
+		decButton = new JButton (".");
+		equButton = new JButton ("=");
+		delButton = new JButton ("Apagar");
+		clrButton = new JButton ("Limpar");
+		
+		functionButtons[0] = addButton;
+		functionButtons[1] = subButton;
+		functionButtons[2] = mulButton;
+		functionButtons[3] = divButton;
+		functionButtons[4] = decButton;
+		functionButtons[5] = equButton;
+		functionButtons[6] = delButton;
+		functionButtons[7] = clrButton;
+		
+		for (int i = 0; i<8; i++) {
+			functionButtons[i].addActionListener(this);
+			functionButtons[i].setFont(myFont);
+			functionButtons[i].setFocusable(false);	
+		}
+		
+		for (int i = 0; i<10; i++) {
+			numberButtons [i] = new JButton(String.valueOf(i));
+			numberButtons [i].addActionListener(this);
+			numberButtons [i].setFont(myFont);
+			numberButtons [i].setFocusable(false);	
+		}
+		
+		delButton.setBounds(50,430,145,50);
+		clrButton.setBounds(205,430,145,50);
+		
+		panel = new JPanel();
+		panel.setBounds(50,100,300,300);
+		panel.setLayout(new GridLayout(4,4,10,10));
+		
+		panel.add(numberButtons[1]);
+		panel.add(numberButtons[2]);
+		panel.add(numberButtons[3]);
+		panel.add(addButton);
+		panel.add(numberButtons[4]);
+		panel.add(numberButtons[5]);
+		panel.add(numberButtons[6]);
+		panel.add(subButton);
+		panel.add(numberButtons[7]);
+		panel.add(numberButtons[8]);
+		panel.add(numberButtons[9]);
+		panel.add(mulButton);
+		panel.add(decButton);
+		panel.add(numberButtons[0]);
+		panel.add(equButton);
+		panel.add(divButton);
+		
+		frame.add(panel);
+		frame.add(delButton);
+		frame.add(clrButton);
+		frame.add(textfield);
+		frame.setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calculator calc = new Calculator();
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	 for (int i = 0; i < 10; i++) {
+		 if(e.getSource() == numberButtons[i]) {
+			 JTextField textfield = new JTextField();
+			 
+		textfield.setText(textfield.getText().concat(String.valueOf(i)));
+	}
+		
+	 }
+	}
+}
